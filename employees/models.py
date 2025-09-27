081bc736cee120b334c3f80411dc6def912df8de
Mamata Singh <mamata.singh@privado.ai>
2025-09-27 13:45:15 +0530

    PHASE 1 COMPLETED DOCUMENTATION AND BASIC SETUP ALONG WITH MIGRATION

---
+++ employees/models.py
@@ -0,0 +1,14 @@
+# apps/employees/models.py
+from django.db import models
+from departments.models import Department
+
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=20, unique=True)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    position = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    hire_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
